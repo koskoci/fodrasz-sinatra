a265facbc024d9df488a79e3520ad4e8b6729f9d
Balázs Koskóci <balazs.koskoci@vodafone.com>
2017-11-12 21:42:37 +0000

    basic smtp setup

--- app.rb
+++ app.rb
@@ -7,6 +7,15 @@ class Fodrasz < Sinatra::Base
   register Padrino::Mailer
 
   set :protect_from_csrf, true
+  set :delivery_method, :smtp => {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'stadionok.fodraszat',
+    :user_name            => 'stadionok.fodraszat.dev',
+    :password             => '***REMOVED***',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true
+  }
 
   before do
     I18n.locale = :hu
@@ -21,6 +30,6 @@ class Fodrasz < Sinatra::Base
   end
 
   post '/email' do
-    "Email should be sent at this point."
+    email(:from => "noreply@stadionok.fodraszat.dev", :to => "***REMOVED***", :subject => "Successfully Registered!", :body => "Test Body")
   end
 end
